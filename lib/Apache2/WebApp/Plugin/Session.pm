3fe8900f63ff7558019f88e2fd5b387bc3264595
Marc S. Brooks <mbrooks@cpan.org>
2010-01-02 04:37:32 +0000

    Import of MBROOKS/Apache2-WebApp-Plugin-Session-0.07 from CPAN.
    gitpan-cpan-distribution: Apache2-WebApp-Plugin-Session
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         MBROOKS/Apache2-WebApp-Plugin-Session-0.07.tar.gz
    gitpan-cpan-author:       MBROOKS
    gitpan-cpan-maturity:     released

--- lib/Apache2/WebApp/Plugin/Session.pm
+++ lib/Apache2/WebApp/Plugin/Session.pm
@@ -22,7 +22,7 @@ use base 'Apache2::WebApp::Plugin';
 use Params::Validate qw( :all );
 use Switch;
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~[  OBJECT METHODS  ]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
